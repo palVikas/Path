97e312cedb5115aad57f05e0a26e68a21434965b
vikaspal <vikaspal@thoughtworks.com>
2015-03-09 15:12:56 +0530

    city displayed its country name like as Bangalore[india] version 5.0 is completed writing more test

--- src/com/path/Path.java
+++ src/com/path/Path.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 class Path {
     public static void main(String[] args) throws Exception{
-        String file = args[0];
-        String source = args[1];
-        String destination = args[2];
+        String pathFile = args[1];
+        String cityFile = args[3];
+        String source = args[4];
+        String destination = args[5];
         ReadFile rdfile = new ReadFile();
-        String content = rdfile.readFile(file);
-        PathUtils paths = new PathUtils(content);
+        String pathContent = rdfile.readFile(pathFile);
+        String cityContent = rdfile.readFile(cityFile);
+        PathUtils paths = new PathUtils(pathContent,cityContent);
         if(!paths.isCityPresent(source)){
             System.out.println("No city named "+source+ " in database");
             return;
