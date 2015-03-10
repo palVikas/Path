af68894d97d02abff5e94bad41db5bab5aa3fb46
vikaspal <vikaspal@thoughtworks.com>
2015-03-10 20:42:13 +0530

    all test passed for version 6

--- test/com/path/PathUtilsTest.java
+++ test/com/path/PathUtilsTest.java
@@ -8,76 +8,69 @@ public class PathUtilsTest {
     ReadFile rdfile = new ReadFile();
     String pathFile = rdfile.readFile("paths.txt");
     String cityFile = rdfile.readFile("cities.txt");
+    Queue<String> path = new LinkedList<String>();
+    List<Queue<String>> expected = new ArrayList<Queue<String>>();
+    PathUtils paths = new PathUtils(pathFile, cityFile);
+    List<String> result = new ArrayList<String>();
+
 
     @Test
     public void pathUtils_returns_true_if__Seoul_is_present_in_database() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
         assertTrue(paths.isCityPresent("Seoul"));
     }
 
     @Test
     public void pathUtils_returns_true_if__dubai_is_present_in_database() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
         assertTrue(paths.isCityPresent("Dubai"));
     }
 
     @Test
     public void pathUtils_returns_false_if__India_is_not_in_database() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
         assertFalse(paths.isCityPresent("India"));
     }
 
     @Test
-    public void isDirectpath_returns_true_if_direct_flight_between_Bejing_to_Tokyo() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertTrue(paths.isDirectPath("Beijing", "Tokyo"));
+    public void getDirectpath_returns__direct_path_flight_between_Bejing_to_Tokyo() {
+         path.add("Beijing");
+         path.add("Tokyo");
+        expected.add(new LinkedList<String>(path));
+        assertEquals(paths.getDirectPath("Beijing", "Tokyo"), expected);
     }
 
     @Test
-    public void isDirectpath_returns_true_if_direct_flight_between_Tokyo_to_Beijing() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertTrue(paths.isDirectPath("Tokyo", "Beijing"));
+    public void getDirectpath_returns_direct_path_between_Tokyo_to_Beijing() {
+        path.add("Tokyo");
+        path.add("Beijing");
+        expected.add(new LinkedList<String>(path));
+        assertEquals(paths.getDirectPath("Tokyo", "Beijing"), expected);
     }
-
+//
     @Test
-    public void isDirectpath_returns_true_if_direct_flight_between_Singapore_to_Dubai() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertTrue(paths.isDirectPath("Singapore", "Dubai"));
+    public void getDirectpath__gives_path_direct_between_Singapore_to_Dubai() {
+        path.add("Singapore");
+        path.add("Dubai");
+        expected.add(new LinkedList<String>(path));
+        assertEquals(paths.getDirectPath("Singapore", "Dubai"), expected);
     }
-
-    @Test
-    public void Directpath_returns_false_if_no_direct_flight_between_Singapore_to_Chennai() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertFalse(paths.isDirectPath("Singapore", "Chennai"));
-    }
-
-    @Test
-    public void Directpath_returns_true_if_flight_between_bangalore_to_tokyo() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertTrue(paths.isDirectPath("Bangalore", "Tokyo"));
-    }
-
-    @Test
-    public void Directpath_returns_false_if_no_flight_between_bangalore_to_chennai() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertFalse(paths.isDirectPath("Bangalore", "Chennai"));
-    }
-
     @Test
     public void getFullPath_returns_the_path_between_bangalore_to_Seoul() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertEquals(paths.getFullPath("Bangalore", "Seoul"), "Bangalore[India]-->Singapore[Singapore]-->Seoul[South Korea]");
-    }
 
+        result.add("Bangalore[India]-->Singapore[Singapore]-->Seoul[South Korea]");
+        result.add("Bangalore[India]-->Singapore[Singapore]-->Dubai[UAE]-->Seoul[South Korea]");
+
+        assertEquals(paths.getFullPath("Bangalore","Seoul"),result);
+    }
+//
     @Test
     public void getFullPath_gives_the_full_path_between_bangalore_to_Tokyo() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertEquals(paths.getFullPath("Bangalore", "Tokyo"), "Bangalore[India]-->Singapore[Singapore]-->Seoul[South Korea]-->Beijing[China]-->Tokyo[Japan]");
+        result.add("Bangalore[India]-->Singapore[Singapore]-->Seoul[South Korea]-->Beijing[China]-->Tokyo[Japan]");
+        result.add("Bangalore[India]-->Singapore[Singapore]-->Dubai[UAE]-->Seoul[South Korea]-->Beijing[China]-->Tokyo[Japan]");
+        assertEquals(paths.getFullPath("Bangalore", "Tokyo"), result);
     }
-
+//
     @Test
     public void getFullPath_gives_the_full_path_between_Tokyo_to_Bangalore() {
-        PathUtils paths = new PathUtils(pathFile, cityFile);
-        assertEquals(paths.getFullPath("Tokyo", "Bangalore"), "Tokyo[Japan]-->Beijing[China]-->Seoul[South Korea]-->Singapore[Singapore]-->Bangalore[India]");
+        result.add("Tokyo[Japan]-->Beijing[China]-->Seoul[South Korea]-->Singapore[Singapore]-->Bangalore[India]");
+        assertEquals(paths.getFullPath("Tokyo", "Bangalore"),result );
     }
 }
