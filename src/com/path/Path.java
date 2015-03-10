9a78c9ea1ac822ad083b7b576a5852703b1e0da3
vikaspal <vikaspal@thoughtworks.com>
2015-03-10 19:52:01 +0530

    version 6 is working with if user gives -a its gives all related path else its gives single related path

--- src/com/path/Path.java
+++ src/com/path/Path.java
@@ -2,30 +2,36 @@ package com.path;
 
 
 class Path {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String pathFile = args[1];
         String cityFile = args[3];
-        String source = args[args.length-2];
-        String destination = args[args.length-1];
+        String source = args[args.length - 2];
+        String destination = args[args.length - 1];
         ReadFile rdfile = new ReadFile();
         String pathContent = rdfile.readFile(pathFile);
-        if(pathContent == null) {
-            System.out.println("No database named "+pathFile+" found");
+        if (pathContent == null) {
+            System.out.println("No database named " + pathFile + " found");
             return;
         }
         String cityContent = rdfile.readFile(cityFile);
-        if(cityContent == null) {
-            System.out.println("No database named "+cityFile+" found");
+        if (cityContent == null) {
+            System.out.println("No database named " + cityFile + " found");
             return;
         }
-        PathUtils paths = new PathUtils(pathContent,cityContent);
-        if(!paths.isCityPresent(source)){
-            System.out.println("No city named "+source+ " in database");
+        PathUtils paths = new PathUtils(pathContent, cityContent);
+        if (!paths.isCityPresent(source)) {
+            System.out.println("No city named " + source + " in database");
+            return;
         }
-        if(!paths.isCityPresent(destination)){
-            System.out.println("No city named "+destination+ " in database");
+        if (!paths.isCityPresent(destination)) {
+            System.out.println("No city named " + destination + " in database");
+            return;
+        }
+        if(args[4].equals("-a")) {
+            for (String result : paths.getFullPath(source, destination))
+                System.out.println(result);
         }
         else
-            System.out.println(paths.getFullPath(source,destination));
+            System.out.println(paths.getFullPath(source, destination).get(0));
     }
 }
