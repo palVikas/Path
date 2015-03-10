278215d07430ef7aedaa6fc97297558e153b85ee
vikaspal <vikaspal@thoughtworks.com>
2015-03-10 12:59:55 +0530

    file handling in main page(path.java )

--- src/com/path/Path.java
+++ src/com/path/Path.java
@@ -1,24 +1,29 @@
 package com.path;
 
-import java.util.*;
 
 class Path {
     public static void main(String[] args) throws Exception{
         String pathFile = args[1];
         String cityFile = args[3];
-        String source = args[4];
-        String destination = args[5];
+        String source = args[args.length-2];
+        String destination = args[args.length-1];
         ReadFile rdfile = new ReadFile();
         String pathContent = rdfile.readFile(pathFile);
+        if(pathContent == null) {
+            System.out.println("No database named "+pathFile+" found");
+            return;
+        }
         String cityContent = rdfile.readFile(cityFile);
+        if(cityContent == null) {
+            System.out.println("No database named "+cityFile+" found");
+            return;
+        }
         PathUtils paths = new PathUtils(pathContent,cityContent);
         if(!paths.isCityPresent(source)){
             System.out.println("No city named "+source+ " in database");
-            return;
         }
         if(!paths.isCityPresent(destination)){
             System.out.println("No city named "+destination+ " in database");
-            return;
         }
         else
             System.out.println(paths.getFullPath(source,destination));
