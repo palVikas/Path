36eee4bf60b24e6689081adb18ae9cd12f8a39f7
vikaspal <vikaspal@thoughtworks.com>
2015-03-16 21:12:32 +0530

    all test are passed for get totalCost method

--- test/com/path/PathUtilsTest.java
+++ test/com/path/PathUtilsTest.java
@@ -6,12 +6,13 @@ import java.util.*;
 
 public class PathUtilsTest {
     ReadFile rdfile = new ReadFile();
-    String pathFile = rdfile.readFile("paths.txt");
+    String pathFile = rdfile.readFile("test/com/path/paths.txt");
     String cityFile = rdfile.readFile("cities.txt");
     Queue<String> path = new LinkedList<String>();
     List<Queue<String>> expected = new ArrayList<Queue<String>>();
     PathUtils paths = new PathUtils(pathFile, cityFile);
     List<String> result = new ArrayList<String>();
+    List<Integer>result1 = new ArrayList<Integer>();
 
 
     @Test
@@ -60,17 +61,34 @@ public class PathUtilsTest {
 
         assertEquals(paths.getFullPath("Bangalore","Seoul"),result);
     }
-//
     @Test
     public void getFullPath_gives_the_full_path_between_bangalore_to_Tokyo() {
         result.add("Bangalore[India]-->Singapore[Singapore]-->Seoul[South Korea]-->Beijing[China]-->Tokyo[Japan]");
         result.add("Bangalore[India]-->Singapore[Singapore]-->Dubai[UAE]-->Seoul[South Korea]-->Beijing[China]-->Tokyo[Japan]");
         assertEquals(paths.getFullPath("Bangalore", "Tokyo"), result);
     }
-//
     @Test
     public void getFullPath_gives_the_full_path_between_Tokyo_to_Bangalore() {
         result.add("Tokyo[Japan]-->Beijing[China]-->Seoul[South Korea]-->Singapore[Singapore]-->Bangalore[India]");
         assertEquals(paths.getFullPath("Tokyo", "Bangalore"),result );
+    }
+    @Test
+    public void TotalCost_gives_the_fullCost_between_Bangalore_To_Singapore() {
+         result1.add(7000);
+        assertEquals(paths.TotalCost("Bangalore", "Singapore"), result1);
+
+    }
+    @Test
+    public void TotalCost_gives_the_fullCost_between_Bangalore_To_Dubai() {
+        result1.add(12500);
+        assertEquals(paths.TotalCost("Bangalore", "Dubai"),result1 );
+
+    }
+    @Test
+    public void TotalCost_gives_the_fullCost_between_Bangalore_To_Tokyo() {
+        result1.add(23000);
+        result1.add(31500);
+        assertEquals(paths.TotalCost("Bangalore", "Tokyo"),result1 );
+
     }
 }
